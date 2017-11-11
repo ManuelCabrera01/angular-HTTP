98b6435c013d554930bd39e61270c09da1cb3c7e
Manuel <m.a.c.p90@gmail.com>
2017-11-11 16:54:03 -0500

    fixed app

--- src/app/jokes/jokes/jokes.component.ts
+++ src/app/jokes/jokes/jokes.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { JokesService } from '../jokes.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/toPromise';
+
+
 
 
 @Component({
   selector: 'app-jokes',
   templateUrl: './jokes.component.html',
   styleUrls: ['./jokes.component.css'],
-  encapsulation: ViewEncapsulation.None,
+
   providers: [JokesService]
 })
 export class JokesComponent implements OnInit {
-  joke$: Observable<string>;
+   joke: Promise<string>;
+
 constructor(private jokes: JokesService) {}
 
-   ngOnInit() {
-     this.joke$ = Observable
-       .fromEvent<MouseEvent>(document.getElementById('joke-btn'), 'click')
-       .throttleTime(1000)
-       .switchMap(
-         (e: MouseEvent) => this.jokes.getRandom()
-       );
- }
+ngOnInit() {}
+
+getRandomJoke() {
+ this.joke = this.jokes.getRandom();
+}
 }
